aa82ddf3368de289e026e3469e0d3e5b18f6701f
Nehalokesh <nehal19697@gmail.com>
2024-03-18 12:38:14 +0530

    Online Auction Application commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const connectDatabase = require('./utils/database');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 
 connectDatabase()
